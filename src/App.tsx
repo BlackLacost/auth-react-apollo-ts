db565095a53fa8dd08210634c3780cdb2fdf327e
Ilya Lisin <blacklacost@gmail.com>
2022-02-05 14:39:54 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from '@apollo/client'
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(gql`
+    query Users {
+      users {
+        id
+        username
+        name
+      }
+    }
+  `)
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error</div>
+
+  return <div>{JSON.stringify(data)}</div>
 }
 
-export default App;
+export default App
